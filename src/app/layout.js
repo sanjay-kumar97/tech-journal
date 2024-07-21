6d3259f23d4c6e917b339406b6485c4dcb20cd1f
VarshanSujith <varshansujith@gmail.com>
2024-07-21 14:50:13 +0530

    User authentication completed using Clerk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SignedIn, ClerkProvider, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="bg-white">
+          <header className="flex justify-between p-4">
+            <h1 className="text-black">Tech Journal</h1>
+            <UserButton showName />
+          </header>
+          <main className="flex item-center justify-center">
+            <SignedOut>
+              <SignIn routing="hash" />
+            </SignedOut>
+            <SignedIn>
+              {children}
+            </SignedIn>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
